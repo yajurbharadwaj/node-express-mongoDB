3cc92ba38cbe66ada22cdc449c27abfc30312e64
Yajur Bharadwaj <yajurnb@gmail.com>
2023-08-29 15:02:05 +0530

    jwt authentification
    this folder consists of the programs related to the jwt authentication where we apply JWT Authentication to our Node JS and Express REST API routes in the most secure way possible to avoid XSS and CSRF attacks.

--- authController.js
+++ authController.js
@@ -3,6 +3,12 @@ const usersDB = {
     setUsers: function (data) { this.users = data }
 }
 const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
 const handleLogin = async(req, res) => {
     const { user, pwd } = req.body;    
     if(!user || !pwd) return  res.status(400).json({'message': 'username and password required'})
@@ -12,7 +18,26 @@ const handleLogin = async(req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password);
     if(match) {
         //create JWTs
-         res.json({'success' : `User ${user} is logged in!`})
+    const accessToken = jwt.sign(
+        {"username": foundUser.username},
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : '30s'} 
+    );
+    const refreshToken = jwt.sign(
+        {"username": foundUser.username},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn : '1d'} 
+    );
+    //saving refresh token with current user
+    const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+    const currentUSer = { ...foundUser,refreshToken };
+    usersDB.setUsers([...otherUsers, currentUSer]);
+    await fsPromises.writeFile(
+        path.join(__dirname, '..', 'model', 'users.json'),
+        JSON.stringify(usersDB.users)
+    );
+         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'none', secure: true, maxAge : 24 * 60 * 60 * 1000});
+         res.json({ accessToken })
     }  else {
         res.sendStatus(401);
     }
